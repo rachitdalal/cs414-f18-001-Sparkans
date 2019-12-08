bcedc779fe51f9cbaadb30054ef3886d3a6957ae
sush1996 <sush1996@colostate.edu>
2019-12-07 19:40:08 -0700

    Minister Test check it out!!!!

--- server/src/test/java/com/sparkans/banqi/game/MinisterTest.java
+++ server/src/test/java/com/sparkans/banqi/game/MinisterTest.java
@@ -12,4 +12,30 @@ public class MinisterTest {
 	void init() {
 		board = new BanqiBoard();
 	}
+
+	@Test
+	void ministerPlacedOnBoard() throws IllegalPositionException,IllegalMoveException{
+		Minister cR = new Minister(board, BanqiPiece.Color.RED);
+		board.placePiece(cR,"c2");
+		board.move("c2","c7");
+		assertTrue(board.getPiece("c2").legalMoves().containsAll(Arrays.asList("c3","c4","c5","c6","c7")));
+
+	}
+	@Test
+	public void MinisterDoNotAttackSameColor() throws IllegalMoveException,IllegalPositionException {
+		Minister cR = new Minister(board, BanqiPiece.Color.RED);
+		board.placePiece(cR,"c3");
+		Soldier sR = new Soldier(board, BanqiPiece.Color.RED);
+		board.placePiece(sR,"b3");
+		assertFalse(board.getPiece("c3").legalMoves().contains("b3"));
+	}
+
+	@Test
+	public void MinisterCannotAttackHigherRankThatOfItself() throws IllegalMoveException,IllegalPositionException{
+		Minister cR = new Minister(board, BanqiPiece.Color.RED);
+		board.placePiece(cR,"a5");
+		Horse hW = new Horse(board, BanqiPiece.Color.WHITE);
+		board.placePiece(hW,"a4");
+		assertFalse(board.getPiece("a5").legalMoves().contains("a4"));
+	}
 }
