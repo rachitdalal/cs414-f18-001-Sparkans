bc5bab48b9e15002de48ca58d6653f7612dbd5f3
rani <nisharani10696@gmail.com>
2019-12-08 16:07:04 -0700

    game rule component added

---
+++ Banqi-UI/src/app/game-rule/game-rule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-rule',
+  templateUrl: './game-rule.component.html',
+  styleUrls: ['./game-rule.component.css']
+})
+export class GameRuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
