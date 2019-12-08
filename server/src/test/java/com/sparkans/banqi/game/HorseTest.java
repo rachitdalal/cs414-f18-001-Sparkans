7d5139a4543c7c94ae7988e46b68681916b74829
sush1996 <sush1996@colostate.edu>
2019-12-07 19:39:19 -0700

    Horse Test!!!!!

--- server/src/test/java/com/sparkans/banqi/game/HorseTest.java
+++ server/src/test/java/com/sparkans/banqi/game/HorseTest.java
@@ -13,4 +13,39 @@ public class HorseTest {
 	void init() {
 		board = new BanqiBoard();
 	}
+	@Test
+	void horseKills() throws IllegalPositionException,IllegalMoveException{
+		Cannon cW = new Cannon(board, BanqiPiece.Color.WHITE);
+		board.placePiece(cW,"b6");
+		Horse hR = new Horse(board, BanqiPiece.Color.RED);
+		board.placePiece(hR,"b5");
+		assertTrue(board.getPiece("b5").legalMoves().contains("b6"));
+	}
+
+	@Test
+	public void horseDoNotAttackSameColor() throws IllegalMoveException,IllegalPositionException {
+		Horse hR = new Horse(board, BanqiPiece.Color.RED);
+		board.placePiece(hR,"c3");
+		Soldier sR = new Soldier(board, BanqiPiece.Color.RED);
+		board.placePiece(sR,"b3");
+		assertFalse(board.getPiece("c3").legalMoves().contains("b3"));
+	}
+
+	@Test
+	public void horseCannotMoveDiagonally() throws IllegalPositionException, IllegalMoveException{
+		Horse hR = new Horse(board, BanqiPiece.Color.RED);
+		board.placePiece(hR,"b4");
+		assertThrows(IllegalMoveException.class, ()->{board.move("b4","a3");});
+	}
+
+	@Test
+	public void  horseCannotAttackHorse()throws IllegalMoveException,IllegalPositionException{
+		Horse aR = new Horse(board, BanqiPiece.Color.RED);
+		board.placePiece(aR,"c1");
+		Horse aW = new Horse(board, BanqiPiece.Color.WHITE);
+		board.placePiece(aW,"c2");
+		assertTrue(board.getPiece("c1").legalMoves().contains("c2"));
+	}
+
+
 }
